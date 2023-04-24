14825f4f11d609667a93c613473b2fd163dc5836
usaone <uddipmitra@gmail.com>
2023-04-24 01:11:11 -0400

    Send Data to View Using ViewData

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -9,5 +9,13 @@ namespace MvcMovie.Controllers
         {
             return View();
         }
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
     }
 }
